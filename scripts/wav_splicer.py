275ff5f90d3f911989f45575ce7274195f944aaa
EarlTheCurl <heimli@outlook.com>
2018-06-28 13:23:50 +0200

    updated script to take arguments

--- scripts/wav_splicer.py
+++ scripts/wav_splicer.py
@@ -1,16 +1,24 @@
-#todo: take filenames as arguments
-
-
 from pydub import AudioSegment
 from pydub.utils import make_chunks
+import argparse
 
-myaudio = AudioSegment.from_file("########.wav" , "wav") # change to name of file you want segmented
-chunk_length_ms = 1000 # length of chunks in ms
+
+parser = argparse.ArgumentParser(description="makes chunks of .wav files")
+parser.add_argument("-l", type=int, default=2000, help="length of chunks in ms (default: 2000ms)")
+parser.add_argument('input_file', help="name of .wav file to splice")
+parser.add_argument('output_file', help="name of outputfile(s)")
+
+args = parser.parse_args()
+
+
+
+myaudio = AudioSegment.from_file(args.input_file , "wav") #name that the chunk files will be given
+chunk_length_ms = args.l # length of chunks in ms
 chunks = make_chunks(myaudio, chunk_length_ms) 
 
-#Export wav files
 
+#Export wav files
 for i, chunk in enumerate(chunks):
-    chunk_name = "#######{0}.wav".format(i)	#output name of chunks
+    chunk_name = args.output_file + "{0}.wav".format(i)	#output name of chunks
 
     chunk.export(chunk_name, format="wav")
